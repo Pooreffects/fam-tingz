5b6475b77915e35ba8d61cb5ebb7a95bec08a738
Mohamed Benabdellah <pooreffects@gmail.com>
2023-01-29 01:10:38 +0000

    refact: refactor components transitions to px instead of Number values

--- src/components/BelovedList.jsx
+++ src/components/BelovedList.jsx
@@ -2,9 +2,9 @@ import { motion } from 'framer-motion';
 export default function BelovedList({ name, love }) {
   return (
     <motion.li
-      initial={{ opacity: 0, translateX: 10 }}
+      initial={{ opacity: 0, translateX: '10' }}
       animate={{ opacity: 1, translateX: 0 }}
-      transition={{ delay: 2, duration: 0.5, ease: 'linear' }}
+      transition={{ delay: 2, duration: 0.5, ease: 'easeIn' }}
       className="flex flex-col items-center gap-4"
     >
       <p className="font-secondary tracking-wide text-xl font-semibold text-slate-300">
